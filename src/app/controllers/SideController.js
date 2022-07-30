2d6957789ebe53207e5a9616763b744e4f129304
tannvv <nvtan.a5@gmail.com>
2022-07-30 13:59:20 +0700

    prettier lint-staged

---
+++ src/app/controllers/SideController.js
@@ -0,0 +1,13 @@
+class SideController {
+    // [GET] /
+    index(req, res) {
+        res.render('home');
+    }
+
+    // [GET] /search
+    search(req, res) {
+        res.render('search');
+    }
+}
+
+module.exports = new SideController();
---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const route = require('./routes');
+const { engine } = require('express-handlebars');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+
+  
+          express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+const port = 3000;
+
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
